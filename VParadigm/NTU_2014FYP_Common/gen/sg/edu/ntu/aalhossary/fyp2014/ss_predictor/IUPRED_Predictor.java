559058d11b41755e2438388e61b26ff9454f7cda
benjaminkong1990 <benjaminkong1990@gmail.com>
2014-12-27 06:17:31 +0000

    added the codes

--- VParadigm/NTU_2014FYP_Common/gen/sg/edu/ntu/aalhossary/fyp2014/ss_predictor/IUPRED_Predictor.java
+++ VParadigm/NTU_2014FYP_Common/gen/sg/edu/ntu/aalhossary/fyp2014/ss_predictor/IUPRED_Predictor.java
@@ -1,7 +1,90 @@
 package sg.edu.ntu.aalhossary.fyp2014.ss_predictor;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.lang.ProcessBuilder.Redirect;
+import java.util.Map;
+import java.util.Vector;
+
 public class IUPRED_Predictor extends Sequence_based_Prediction {
 
-	public void Predict() {
-		throw new UnsupportedOperationException();
+	public static void Predict(String[]args)throws InterruptedException {
+		
+			 
+			
+			
+			ProcessBuilder pb = new ProcessBuilder();
+			
+			pb.directory(new File("/temp/iupred"));
+			
+			
+			String line = null;
+			try {
+
+				assert pb.redirectInput() == Redirect.PIPE;
+				Vector<String> commands = new Vector<String>();
+				commands.add("./iupred");
+				commands.add("--");
+				commands.add("long");
+				Map <String, String> env = pb.environment();
+//				env.put("IUPred_PATH", strLine);
+				
+			/*	Set<String> keySet = environment.keySet();
+				for(String key: keySet){
+					String value = environment.get(key);
+					System.out.println(key+"\t\t:\t"+value);
+				}*/
+			//	commands.add("./iupred");
+			//	commands.add("--");
+			//	commands.add("long");
+				pb.command(commands);
+				pb.redirectOutput(Redirect.PIPE);
+				pb.redirectErrorStream(true);
+				
+				Process p =pb.start();
+				try{
+					FileInputStream fstream = new FileInputStream("/temp/iupred/P53_HUMAN.seq");
+					OutputStream out = p.getOutputStream();
+					
+					int d;
+					while((d = fstream.read()) != -1){
+						out.write(d);
+					}
+					fstream.close();
+					out.close();
+				}
+				catch(FileNotFoundException e){
+					
+				}
+				
+				
+			
+			
+				
+			
+		
+				
+				InputStream is = p.getInputStream();
+				BufferedReader br = new BufferedReader(new InputStreamReader(is));
+				while ((line = br.readLine()) != null)
+						   System.out.println(line);
+				
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			
+			}
+			
+			
+
+			
+		}
+		
 	}
-}
--- VParadigm/NTU_2014FYP_Common/gen/sg/edu/ntu/aalhossary/fyp2014/ss_predictor/STRIDE_Predictor.java
+++ VParadigm/NTU_2014FYP_Common/gen/sg/edu/ntu/aalhossary/fyp2014/ss_predictor/STRIDE_Predictor.java
@@ -1,8 +1,64 @@
 package sg.edu.ntu.aalhossary.fyp2014.ss_predictor;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
 public class STRIDE_Predictor extends Structure_based_Prediction {
 
-	public void Predict() {
-		throw new UnsupportedOperationException();
+	public static void Predict(String []args) {
+		
+
+	        FileReader input = null;
+	        BufferedReader br = null;
+
+	        try {
+
+	            input = new FileReader("/Users/benkong/Documents/FYP/stride/4HHBout");
+	            br = new BufferedReader(input);
+	            String str;
+	          //  Region predicted = new Region();
+	            //ArrayList<Region> Pregion = new ArrayList<Region>();
+	            while ((str = br.readLine()) != null) {
+	            	Pattern p = Pattern.compile("LOC");
+	            	Matcher m =p.matcher(str);
+	            	if (m.find()){
+	            	ArrayList<String> region_list = new ArrayList<String>();
+	            	region_list.add(str);
+	            	for(String sentence:region_list){
+	            		String []splitArray = sentence.split("\\s+");
+	            	//	predicted.Rpred(splitArray[1]);
+	            		//predicted.Rchain(splitArray[4]);
+	            	//	predicted.Rstart(splitArray[2],splitArray[3]);
+	            //		predicted.Rend(splitArray[5], splitArray[6]);
+	            	//	Pregion.add(predicted);
+	            	}
+	            //	Pregion.get(0).printRegion();
+	            	
+	            	}
+	   
+	            }
+	        }
+	        catch (IOException e) {
+	            e.printStackTrace();
+	        }
+
+	        finally {
+
+	            try {
+	                input.close();
+	                br.close();
+	            }
+
+	            catch (IOException x) {
+	                x.printStackTrace();
+	            }
+
+	        }
+	    }
 	}
-}
